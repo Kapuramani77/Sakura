6c75312a6600cd3d6b782afc14fa17997abf6fb7
Kapuramani77 <66730123+Kapuramani77@users.noreply.github.com>
2020-06-11 16:31:36 +0530

    Created  area.c
    The file prints area of the circle in the screen

---
+++ Area.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float r,area;
+printf("Enter the radius of the circle: ");
+scanf("%f",&area);
+area=3.14*r*r;
+printf("\Area of the circle %f",area);
+return 0;
+}
